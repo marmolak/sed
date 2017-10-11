80fc060e644f032a94f7c9a2344dbf9f1ff77b5a
Robin Hack <hack.robin@gmail.com>
2017-10-11 12:16:12 +0200

    game: Fix.

--- game/game.sh
+++ game/game.sh
@@ -10,10 +10,8 @@ sed -n '
         : again;
         g;
         s/south_hell/Ses ted satanova cuba!/; t goprint
-        T nogoprint;
         s/^$/Vitej predatore! Na jihu te ceka peklo. Muzes tam jit prikazem: go south./;
         : goprint;
         P;
-        : nogoprint;
     }
 '
