4692ec40dc8d07bea9f7a5e98b42b35cdf43f18d
Robin Hack <hack.robin@gmail.com>
2017-10-11 12:18:50 +0200

    game: Add choice.

--- game/game.sh
+++ game/game.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 sed -n '
-    /go/ {
+   /go/ {
         /south/ {
             s/.*/south_hell/; h; b again;
         }
-    }
+        /west/ {
+            s/.*/west_enterprise/; h; b again;
+        }
+   }
    /^$/ {
         : again;
         g;
         s/south_hell/Ses ted satanova cuba!/; t goprint
-        s/^$/Vitej predatore! Na jihu te ceka peklo. Muzes tam jit prikazem: go south./;
+        s/west_enterprise/Ses ted Jimova cuba!/; t goprint
+        s/^$/Vitej predatore! Na jihu te ceka peklo. Muzes tam jit prikazem: go south.
+             Muzes jit take pracovat do enterprise prostredi prikazem: go west./;
         : goprint;
         P;
     }
