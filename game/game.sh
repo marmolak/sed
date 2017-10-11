82750aaf45eab7f36a7e6999119122afd514068b
Robin Hack <hack.robin@gmail.com>
2017-10-11 12:33:10 +0200

    game: Fixes, fixes, fixes...

--- game/game.sh
+++ game/game.sh
@@ -3,20 +3,22 @@
 sed -n '
    /go/ {
         /south/ {
-            s/.*/south_hell/; h; b again;
+            s/^.*$/south_hell/; h; b again;
         }
         /west/ {
-            s/.*/west_enterprise/; h; b again;
+            s/^.*$/west_enterprise/; h; b again;
         }
    }
    /^$/ {
         : again;
         g;
-        s/south_hell/Ses ted satanova cuba!/; t goprint
-        s/west_enterprise/Ses ted Jimova cuba!/; t goprint
-        s/^$/Vitej predatore! Na jihu te ceka peklo. Muzes tam jit prikazem: go south.
-             Muzes jit take pracovat do enterprise prostredi prikazem: go west./;
-        : goprint;
-        P;
+        s/south_hell/Ses ted satanova cuba!/;
+        s/west_enterprise/Ses ted Jimova cuba!/;
+        s/^$/Vitej predatore! Na jihu te ceka peklo. Muzes tam jit prikazem: go south. Muzes jit take pracovat do enterprise prostredi prikazem: go west./;
+        t goprint;
+        T noprint;
+
+        : goprint; P;
+        : noprint
     }
 '
